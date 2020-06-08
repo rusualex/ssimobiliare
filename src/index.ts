17487a3d51d408fd09caee7d77bdce974aed8592
rusualex <rusu_alex_geo@yahoo.com>
2020-06-08 11:33:19 +0300

    Alterate id1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { IdMapper } from './services/idMapper.service';
 import { LoginRouter } from './routes/login.router';
 import { ParentRouter } from './routes/parent.router';
 import { RentalRouter } from './routes/rental.router';
@@ -8,6 +7,7 @@ import { LoginService } from './services/login.service';
 import { ResponseWrapperService } from './services/response-wrapper.service';
 import { RentalService } from './services/rental.service';
 import { UserService } from './services/user.service';
+import { IdMapper } from './services/idMapper.service';
 
 const parentRouter: ParentRouter = new ParentRouter();
 const userRouter: UserRouter = new UserRouter();
@@ -18,7 +18,7 @@ const rentalService: RentalService = new RentalService();
 const responseWrapperService: ResponseWrapperService = new ResponseWrapperService();
 const hashService: HashService = new HashService();
 const loginService: LoginService = new LoginService();
-const IdMapper: IdMapper = new IdMapper();
+const idMapper: IdMapper = new IdMapper();
 // if (!config.get('jwtPrivateKey')) {
 //   console.error('FATAL ERROR: jwtPrivateKey is not defined');
 //   process.exit(1);
--- src/services/idMapper.service.ts
+++ src/services/idMapper.service.ts
@@ -1,5 +1,6 @@
 import { IUser } from './../model/user.model';
 export class IdMapper {
+    constructor(){};
     async remapUser(user: IUser) {
         const customUser = user;
         customUser.id = customUser._id;
