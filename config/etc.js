afc89f402e13eea55b4834b9f7422554c1dcc78e
rusualex <rusu_alex_geo@yahoo.com>
2020-06-08 16:23:57 +0300

    Put test

---
+++ config/etc.js
@@ -0,0 +1,65 @@
+this.apartments.push({
+    id: 1,
+    pictureFileName: "app.jpg",
+    name: "Apartment for Rent in Manastur",
+    details: "Very nice zone, 3 Rooms and balcony.",
+    owner: "Popescu Ioan",
+    price: "350€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d6853.983594931433!2d23.55182174159005!3d46.75248516690848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490e661692f1cd%3A0x147e97b23edd308c!2zTcSDbsSDyJl0dXIsIENsdWotTmFwb2Nh!3m2!1d46.7526905!2d23.556113699999997!4m0!5e0!3m2!1sro!2sro!4v1585861751878!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 2,
+    pictureFileName: "studio.jpg",
+    name: "Studio Apartment",
+    details: "Perfect for Students",
+    owner: "Maria Anca",
+    price: "250€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d87429.25874897714!2d23.542482992525574!3d46.781366087198364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490c1f2c7386cb%3A0x4f14b83a2bd331f!2zTcSDcsSDyJl0aSwgQ2x1ai1OYXBvY2E!3m2!1d46.78127!2d23.6126954!4m0!5e0!3m2!1sro!2sro!4v1585862391057!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 3,
+    pictureFileName: "studio2.jpg",
+    name: "Studio Apartment",
+    details: "Good view over the city",
+    owner: "John John",
+    price: "300€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d87447.81348693362!2d23.51865489195774!3d46.7699389116159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490e9d0bb760e3%3A0x1a009cab634afa8!2sCentru%2C%20Cluj-Napoca!3m2!1d46.7698428!2d23.5888673!4m0!5e0!3m2!1sro!2sro!4v1585862474254!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 4,
+    pictureFileName: "2room.jpg",
+    name: "2 Room Apartment in City Center",
+    details: "Very nice positioning.",
+    owner: "Pop Teo",
+    price: "450€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d87447.81348693362!2d23.51865489195774!3d46.7699389116159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490e9d0bb760e3%3A0x1a009cab634afa8!2sCentru%2C%20Cluj-Napoca!3m2!1d46.7698428!2d23.5888673!4m0!5e0!3m2!1sro!2sro!4v1585862474254!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 5,
+    pictureFileName: "balcony.jpg",
+    name: "Apartment with balcony",
+    details: "Wonderful balcony with wonderful view.",
+    owner: "Maria Anca",
+    price: "375€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d87447.81348693362!2d23.51865489195774!3d46.7699389116159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490e9d0bb760e3%3A0x1a009cab634afa8!2sCentru%2C%20Cluj-Napoca!3m2!1d46.7698428!2d23.5888673!4m0!5e0!3m2!1sro!2sro!4v1585862474254!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 6,
+    pictureFileName: "1room.jpg",
+    name: "1 Room in an Apartment",
+    details: "Searching for a student flatmate.",
+    owner: "Theodor Ferdinand",
+    price: "150€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d6853.983594931433!2d23.55182174159005!3d46.75248516690848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490e661692f1cd%3A0x147e97b23edd308c!2zTcSDbsSDyJl0dXIsIENsdWotTmFwb2Nh!3m2!1d46.7526905!2d23.556113699999997!4m0!5e0!3m2!1sro!2sro!4v1585861751878!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+}, {
+    id: 7,
+    pictureFileName: "house.jpg",
+    name: "House for Rent",
+    details: "A house with 4 rooms and 2 floors.",
+    owner: "Pop Teo",
+    price: "650€",
+    mapLink: ""
+}, {
+    id: 8,
+    pictureFileName: "3room.jpg",
+    name: "Apartment for Rent in Marasti",
+    details: "Very friendly neighborhood",
+    owner: "John John",
+    price: "350€",
+    mapLink: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d87429.25874897714!2d23.542482992525574!3d46.781366087198364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m5!1s0x47490c1f2c7386cb%3A0x4f14b83a2bd331f!2zTcSDcsSDyJl0aSwgQ2x1ai1OYXBvY2E!3m2!1d46.78127!2d23.6126954!4m0!5e0!3m2!1sro!2sro!4v1585862391057!5m2!1sro!2sro\" width=\"100%\" height=\"200\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>"
+})
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -21,8 +21,8 @@ export class UserService {
     return new User(user).save();
   }
 
-  async updateUser(user: IUser): Promise<IMongoResponse> {
-    const currentUser: IUser = await this.getUserById(user._id);
+  async updateUser(user: any): Promise<IMongoResponse> {
+    const currentUser: IUser = await this.getUserById(user.id);
     const samePassword: boolean = user.encrypted_password === currentUser.encrypted_password;
 
     if (!samePassword) {
@@ -31,7 +31,7 @@ export class UserService {
 
     // await rentalService.updateNestedUsers(user);
 
-    return User.updateOne({ _id: user._id }, { $set: user });
+    return User.updateOne({ _id: user.id }, { $set: user });
   }
 
   async deleteUserById(userId: string): Promise<IMongoResponse> {
